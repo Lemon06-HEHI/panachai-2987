8271e253d128269161312a10a690ffa471e7b05d
walen <walen6793@gmail.com>
2025-06-01 14:30:22 +0700

    edit

---
+++ src/modules/category/category.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from 'src/entities/category.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class CategoryService {
+  constructor(@InjectRepository(Category) private repo: Repository<Category>) {}
+
+  findall() {}
+}
